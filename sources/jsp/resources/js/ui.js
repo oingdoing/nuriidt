f723aee9d6e75b2e0f3440eaa9cbc8008f5b43eb
samylk <samy.ltk@gmail.com>
2023-03-10 11:05:35 +0900

    메인 페이지 작업

--- sources/jsp/resources/js/ui.js
+++ sources/jsp/resources/js/ui.js
@@ -1,1 +1,18 @@
-$(function () {});
+let $nav = $('.app-neav');
+console.log('testttt');
+
+$(function () {
+  nav.init();
+  console.log('test');
+});
+
+const nav = {
+  init: function () {
+    nav.open();
+  },
+  open: function () {
+    $nav.on('click', '.btn-menu', function () {
+      $(this).toggleClass('is-active');
+    });
+  },
+};
